3b92fad380a9b0a630c23e8c7862d4a6637b3d79
Kang Kyung Koo <mc2e.dev@gmail.com>
2013-07-29 09:56:49 +0900

    change game list server url

--- TheFightingKor/src/main/java/com/appspot/thefightingkor/Server/ServerInfo.java
+++ TheFightingKor/src/main/java/com/appspot/thefightingkor/Server/ServerInfo.java
@@ -5,11 +5,11 @@ package com.appspot.thefightingkor.Server;
  */
 public class ServerInfo {
 
-    public static final String BASE_URL = "http://the-fighting-kor.appspot.com/";
+    private static final String BASE_URL = "http://the-fighting-kor.appspot.com/";
 
     public static final String PLAYER_URL = BASE_URL+"json";
 
-    public static final String GAME_LIST_URL = BASE_URL+"fightinfo?ground=";
+    public static final String GAME_LIST_URL = BASE_URL+"fight?ground=";
 
     public static final String GAME_INFO_URL = BASE_URL+"fight?gid=";
 }
